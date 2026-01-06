9a71e174b9a77aea2044e7c7b9128933b458f049
Ankitagouda07 <harshithkarthan07@gmail.com>
2026-01-06 02:53:18 -0500

    Delete Script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-function searchCafes() {
-    let input = document.getElementById("searchInput").value.toLowerCase();
-    let cafes = document.getElementsByClassName("cafe");
-
-    for (let i = 0; i < cafes.length; i++) {
-        let text = cafes[i].innerText.toLowerCase();
-        cafes[i].style.display = text.includes(input) ? "block" : "none";
-    }
-}
